b65d16f864898b8785e1b6673b33a3764f93d4cb
lyssenko <mutecino@live.com>
2019-10-09 14:39:35 +0600

    Task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.a = min;
+        this.b = max;
     }
 
     guess() {
-
+        this.c = Math.ceil((this.b + this.a) / 2);
+        return this.c;
     }
 
     lower() {
-
+        this.b = this.c;
     }
 
     greater() {
-
+        this.a = this.c;
     }
 }
 
